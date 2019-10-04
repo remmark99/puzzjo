8a170d133e80fdbb3da39d1038c39bcaf3a71e0a
victor-dosca <victor-dosca@yandex-team.ru>
2019-10-05 01:31:32 +0300

    Updates

---
+++ precache-manifest.2c506875ffcfe134e59872081d2384ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef145e6f73228877738e1f738af770f1",
+    "url": "/puzzjo/index.html"
+  },
+  {
+    "revision": "54f85f607e979c7196c8",
+    "url": "/puzzjo/static/css/main.7279882f.chunk.css"
+  },
+  {
+    "revision": "6672be5cdc638ec83d31",
+    "url": "/puzzjo/static/js/2.4a3b2bfa.chunk.js"
+  },
+  {
+    "revision": "54f85f607e979c7196c8",
+    "url": "/puzzjo/static/js/main.b6330270.chunk.js"
+  },
+  {
+    "revision": "f810aaca9241b52f4ec1",
+    "url": "/puzzjo/static/js/runtime-main.e01b32a9.js"
+  }
+]);
